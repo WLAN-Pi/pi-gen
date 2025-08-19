26838db7c35e9cc78fd1d47d870ebd2469ea2113
Adrian Granados <adriangranados@users.noreply.github.com>
2025-08-19 14:20:01 -0400

    Re-enabled backports repo (from Debian Archives)

--- wlanpi0/00-install-packages/04-run.sh
+++ wlanpi0/00-install-packages/04-run.sh
@@ -12,8 +12,8 @@ on_chroot <<CHEOF
  	# curl -s https://packagecloud.io/install/repositories/wlanpi/dev/script.deb.sh | bash
 
 	# Add Bullseye Backports repository
-	# echo 'deb http://deb.debian.org/debian bullseye-backports main' | tee /etc/apt/sources.list.d/bullseye-backports.list
-	# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+	echo 'deb http://archive.debian.org/debian bullseye-backports main' | tee /etc/apt/sources.list.d/bullseye-backports.list
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
 
 	# Add Grafana repository
 	if [ ! -f /etc/apt/sources.list.d/grafana.list ]; then
