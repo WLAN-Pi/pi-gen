5eb47a8b3d99410989588cf0778962df5ac87078
jsz <josh@joshschmelzle.com>
2025-09-26 08:40:05 -0400

    Fix grafana repo

--- wlanpi0/00-install-packages/04-run.sh
+++ wlanpi0/00-install-packages/04-run.sh
@@ -18,8 +18,9 @@ on_chroot <<CHEOF
 	# Add Grafana repository
 	if [ ! -f /etc/apt/sources.list.d/grafana.list ]; then
 		echo "Adding grafana repository"
-		wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
-		echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
+		mkdir -p /etc/apt/keyrings/
+		wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor > /etc/apt/keyrings/grafana.gpg
+		echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
 	fi
 
 	# Add InfluxData repository (influxdb, influxdb2, telegraf, chronograf)
